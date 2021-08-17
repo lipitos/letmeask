ab22bf7cf246514c99769ea84940c93d38783575
lipitos <fneves.si@gmail.com>
2021-08-17 11:03:06 -0300

    Estilização de páginas de home e new room. Criação de login.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { BrowserRouter, Route } from "react-router-dom";
 import { Button } from "./components/Button";
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <div>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,44 @@
+import { useHistory } from "react-router-dom";
+import { auth, firebase } from "../services/firebase";
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
+
+export function Home() {
+  const history = useHistory();
+
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then((result) => {
+      console.log(result);
+      history.push("/rooms/new");
+    });
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire suas dúvidas em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logo" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo Google" />
+            Crie uma sala com o Google
+          </button>
+          <div className="separator">Entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
